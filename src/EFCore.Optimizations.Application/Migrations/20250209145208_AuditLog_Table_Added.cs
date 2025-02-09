1134a10c5a5f39f6e6d7f426ed0ce362fa6314e3
Tanjeer <tbh.tilok@live.com>
2025-02-09 16:13:31 +0100

    audit log interceptor added

---
+++ src/EFCore.Optimizations.Application/Migrations/20250209145208_AuditLog_Table_Added.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore.Optimizations.Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuditLog_Table_Added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TableName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Operation = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    PrimaryKey = table.Column<int>(type: "int", nullable: false),
+                    OldValues = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    NewValues = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    User = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuditLogs", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_Email",
+                table: "Profiles",
+                column: "Email");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuditLogs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_Email",
+                table: "Profiles");
+        }
+    }
+}
